3c054316240a3c13ee2c7c50cf45e8c6608fc83b
nab138 <nab@nabdev.me>
2025-05-09 21:32:51 -0400

    Create Ecliptic Core

---
+++ EclipticCore/src/client/java/me/nabdev/ecliptic/core/ClientInitializer.java
@@ -0,0 +1,12 @@
+package me.nabdev.ecliptic.core;
+
+import com.github.puzzle.core.loader.launch.provider.mod.entrypoint.impls.ClientModInitializer;
+
+public class ClientInitializer implements ClientModInitializer {
+
+    @Override
+    public void onInit() {
+        Constants.LOGGER.info("Hello From INIT");
+    }
+
+}
---
+++ EclipticCore/src/main/java/me/nabdev/ecliptic/core/Constants.java
@@ -0,0 +1,13 @@
+package me.nabdev.ecliptic.core;
+
+import finalforeach.cosmicreach.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Constants {
+
+    public static final String MOD_ID = "ecliptic-core";
+    public static final Identifier MOD_NAME = Identifier.of(MOD_ID, "EclipticCore");
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+}
